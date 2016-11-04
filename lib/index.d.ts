f83c3a40650d25dba82e16177f7a6900a43c14a6
zhouhoujun <houjunzhou@skspruce.net>
2016-11-04 18:04:03 +0800

    Comments doc in coding.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,32 @@
 import { IDynamicTaskOption, IDynamicTasks } from 'development-core';
 export interface ITsTaskOption {
+    /**
+     * ts tsconfig.json file path.
+     *
+     * @type {sring}
+     * @memberOf ITsTaskOption
+     */
     tsconfigFile?: string;
+    /**
+     * ts compile config.
+     *
+     * @type {*}
+     * @memberOf ITsTaskOption
+     */
     tsconfig?: any;
+    /**
+     * babel 6 option.
+     *
+     * @type {*}
+     * @memberOf ITsTaskOption
+     */
     babelOption: any;
+    /**
+     * sourceMaps path.
+     *
+     * @type {string}
+     * @memberOf ITsTaskOption
+     */
     sourceMaps: string;
 }
 export declare class TsTasks implements IDynamicTasks {
