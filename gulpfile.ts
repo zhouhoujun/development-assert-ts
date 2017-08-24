80a79ce38524d2a79ed0c9b5f22feade8dda1f0b
zhouhoujun <freebirds8281@hotmail.com>
2017-08-24 16:10:03 +0800

    update package,  v2.0.4

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 
 const del = require('del');
 
-gulp.task('build', () => {
+gulp.task('start', () => {
     let env: IEnvOption = minimist(process.argv.slice(2), {
         string: 'env',
         default: { env: process.env.NODE_ENV || 'development' }
